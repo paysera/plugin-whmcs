741fedb597160cbe500efd0f1a189ef1d119c9c3
Style Mistake <stylemistake@gmail.com>
2014-03-08 03:33:25 +0200

    Squash 4db3051..2e44a52
    * Fix bugs caused by forceful redirect (invoice viewer was redirecting to gateway instead of doing nothing).
    * Complete rewrite of callback script using WHMCS built-in functions
    * Add correct response on status == 1
    * More response fixes

--- modules/gateways/callback/paysera.php
+++ modules/gateways/callback/paysera.php
@@ -5,150 +5,64 @@ include("../../../includes/functions.php");
 include("../../../includes/gatewayfunctions.php");
 include("../../../includes/invoicefunctions.php");
 
-require_once('../vendor/webtopay/libwebtopay/WebToPay.php');
+require_once "../vendor/webtopay/libwebtopay/WebToPay.php";
 
 $gatewaymodule = "paysera";
-$GATEWAY       = getGatewayVariables($gatewaymodule);
+$GATEWAY = getGatewayVariables( $gatewaymodule );
 
+if ( ! $GATEWAY["type"] ) die("Module Not Activated");
 
-
-if (!$GATEWAY["type"])
-    die("Module Not Activated");
-
-try {
-    $response = WebToPay::validateAndParseData($_REQUEST, $GATEWAY['projectID'], $GATEWAY['projectPass']);
-
-    $orderid   = intval(mysql_real_escape_string($response['orderid']));
-    $invoiceid = checkCbInvoiceID($orderid, $GATEWAY["paysera"]);
-
-
-
-    if (isset($_REQUEST['accepturl'])) {
-        $invoiceid = checkCbInvoiceID($orderid, $GATEWAY["paysera"]);
-        logTransaction($GATEWAY["name"], $_REQUEST, "Successful");
-        header("Location: /clientarea.php"); exit();
-    } else {
-
-        //Admin username for API
-        if (!$GATEWAY["adminusername"]) {
-            die("Admin login name not set");
-        } else {
-            $adminusername = $GATEWAY["adminusername"];
-        }
-
-        $orderAmount = '';
-        $result      = mysql_query("SELECT total FROM tblinvoices WHERE id = " . $orderid);
-        $orderAmount = mysql_result($result, 0);
-
-        if (isset($GATEWAY['convertto'])) {
-            $result = mysql_query("SELECT rate FROM tblcurrencies WHERE code = '" . $response['currency'] . "'");
-            $rate   = '';
-            while ($row = mysql_fetch_array($result)) {
-                $rate = $row['rate'];
-            }
-            $orderAmount *= $rate;
-        }
-
-
-        if ($response['status'] == 1) {
-
-            //Check amount
-            if (intval(number_format($orderAmount, 2, '', '')) > $response['amount']) {
-                logTransaction($GATEWAY["paymentmethod"], $_REQUEST, "[CALLBACK]Unsuccessful, bad amount");
-                exit('Bad amount!');
-            }
-
-            //Get real invoice id's if there are multiple invoices paid with one invoice. Check for duplicate callback.
-
-
- 
-            $query = "SELECT * from tblorders where invoiceid =".$orderid;
-
-            $orders_result = mysql_query($query);
-            while ($result = mysql_fetch_array($orders_result)) {
-
-                if ($result['status'] == 'Pending') {
-
-                    $value['status']    = "Paid";
-                    $value['invoiceid'] = $orderid;
-                    localAPI("updateinvoice", $value, $adminusername);
-
-                    $type = localAPI("getinvoice", $value, $adminusername);
-                    if ($type['items']['item']['0']['type'] == "Invoice") {
-                        mysql_query("UPDATE tblorders SET status = 'Processed' WHERE invoiceid = " . $orderid);
-                        mysql_query("UPDATE tblinvoices SET status = 'Paid' WHERE id = " . $orderid); 
-
-                        //In case if user closed redirect from paysera to accept page but payment recieved
-                        mysql_query("UPDATE tblinvoiceitems SET amount = '0.00' WHERE invoiceid = " . $orderid);
-                        $value['invoiceid'] = $orderid;
-                        localAPI("updateinvoice", $value, $adminusername);
-                        logTransaction($GATEWAY["name"], $_REQUEST, "[CALLBACK]Successful");
-                    } else {
-                        mysql_query("UPDATE tblorders SET status = 'Processed' WHERE invoiceid = " . $orderid);
-			            mysql_query("UPDATE tblinvoices SET status = 'Paid', `datepaid` = '".date("Y-m-d H:i:s")."' WHERE id = " . $orderid);
-	                    addInvoicePayment($orderid,$result['ordernum'],$result['amount'],0,$gatewaymodule);
-	                    logTransaction($GATEWAY["name"],$_POST,"Successful");
-
-
-	                    $result = select_query("tblinvoiceitems", "", array("invoiceid" => $orderid, "type" => "Hosting"));
-	                    $data = mysql_fetch_array($result);
-	                    $relid = $data["relid"];
-                        $term = array('Monthly'=> 1, 'Quarterly' => 3, 'Semi-Annually' => 6, 'Annually' => 12, 'Biennially'=> 24, 'Triennially' => 36);
-
-                        if($data){
-                            $result2 = select_query("tblorders", "", array("invoiceid" => $orderid));
-                            $data2 = mysql_fetch_array($result2);
-
-                            $result3 = select_query("tblhosting", "", array("orderid" => $data2["id"]));
-                            $data3 = mysql_fetch_array($result3);
-
-
-                            mysql_query("UPDATE tblhosting SET `nextduedate` = '".date("Y-m-d H:i:s", strtotime($data3['regdate'].'+'.$term[$data3['billingcycle']].' month'))."',
-                        `nextinvoicedate` = '".date("Y-m-d H:i:s", (strtotime($data3['regdate'].'+'.$term[$data3['billingcycle']].' month') - (5*24*60*60)))."' WHERE orderid = " . $data2["id"]);
-
-                            $values["accountid"] = $data3['id'];
-
-                            $results = localAPI("modulecreate" ,$values ,$adminusername); exit("OK");
-                        }
-
-
-
-	                    $result = select_query("tblinvoiceitems", "", array("invoiceid" => $orderid, "type" => "DomainRegister"));
-	                    $data = mysql_fetch_array($result); //print_r($data); die;
-	                    $relid = $data["relid"];
-
-
-                        if($data){
-                            mysql_query("UPDATE tbldomains SET registrar = 'resellerclub' WHERE id = " . $relid);
-                            $values["domainid"] = $relid;
-
-                            $results = localAPI("domainregister" ,$values ,$adminusername);  exit("OK");
-
-                            mysql_query("update `tbldomains` set `nextduedate` = `expirydate`, `nextinvoicedate` = `expirydate` where id = " . $relid);
-                        }
-
-
-
-
-                        logTransaction($GATEWAY["name"], $_REQUEST, "[CALLBACK]Successful");
-                    }
-                } else {
-                    exit("OK");
-                }
-            }
-        }
-
-    }
-
-    exit('OK');
-} catch (Exception $e) {
-    logTransaction($GATEWAY["name"], $_REQUEST, "[CALLBACK]Unsuccessful");
-    exit(get_class($e) . ': ' . $e->getMessage());
+function _log( $msg, $data = null ) {
+    logTransaction( $GATEWAY["name"], var_export( $data, true ), $msg );
 }
 
-function d($d) {
-    echo "<pre>";
-    print_r($d);
-    echo "</pre>";
-    die();
+try {
+
+    $response = WebToPay::validateAndParseData( $_REQUEST, $GATEWAY['projectID'], $GATEWAY['projectPass'] );
+    $orderid = intval( mysql_real_escape_string( $response['orderid'] ) );
+    $invoiceid = checkCbInvoiceID( $orderid, $GATEWAY["paysera"] );
+    $transid = $response["requestid"];
+    $amount = $response["amount"] / 100;
+
+    // Check if we want to redirect user to the invoice
+    if ( isset( $_REQUEST['accepturl'] ) ) {
+        header( "Location: /viewinvoice.php?id=" . $invoiceid );
+        exit;
+    }
+
+    // Convert currency
+    if ( isset( $GATEWAY['convertto'] ) ) {
+        $result = mysql_query("SELECT rate FROM tblcurrencies WHERE code = '" . $response['currency'] . "'");
+        while ( $row = mysql_fetch_array($result) ) $rate = $row['rate'];
+        $amount *= $rate;
+    }
+
+    $amount = number_format( $amount, 2, ".", "" );
+
+    // Check status is something other, than unpaid (0)
+    if ( $response["status"] > 0 ) {
+        if ( $amount <= 0 ) {
+            _log( "[Fail] Paid zero amount", $response );
+            exit;
+        }
+        if ( $response["status"] == 2 ) {
+            _log( "[Fail] Paid, but still processing", $response );
+            exit;
+        }
+        checkCbTransId( $transid ); // Check transid isn't in database, otherwise exit
+        addInvoicePayment( $invoiceid, $transid, $amount, 0, $gatewaymodule );
+        _log( "[Success] Paid", array( "amount" => $amount, "response" => $response ) );
+        exit("OK");
+    }
+
+    // Log failure if everything else fails
+    _log( "[Fail] Unpaid", $response );
+    exit;
+
+} catch ( Exception $e ) {
+    _log( "[Error] Exception", array(
+        "exception" => get_class($e) . ": " . $e->getMessage(),
+        "request" => $_REQUEST
+    ));
+    exit;
 }
--- modules/gateways/paysera.php
+++ modules/gateways/paysera.php
@@ -1,38 +1,28 @@
 <?php
 
-require_once('vendor/webtopay/libwebtopay/WebToPay.php');
+require_once "vendor/webtopay/libwebtopay/WebToPay.php";
 
 function paysera_config() {
-    $configarray = array(
-        "FriendlyName"  => array("Type" => "System", "Value" => "Paysera.com / Mokejimai.lt"),
-        "adminusername" => array("FriendlyName" => "Admin Username", "Type" => "text", "Size" => "15", "Description" => "Enter admin username"),
-        "projectID"     => array("FriendlyName" => "Project ID", "Type" => "text", "Size" => "10", "Description" => "Enter unique Project ID",),
-        "projectPass"   => array("FriendlyName" => "Project password", "Type" => "text", "Size" => "32", "Description" => "Enter unique sign password",),
-        "testmode"      => array("FriendlyName" => "Test Mode", "Type" => "yesno", "Description" => "Tick this to enable test mode",),
+    return array(
+        "FriendlyName" => array("Type" => "System", "Value" => "Paysera.com / Mokejimai.lt"),
+        "projectID"    => array("FriendlyName" => "Project ID", "Type" => "text", "Size" => "10", "Description" => "Enter unique Project ID",),
+        "projectPass"  => array("FriendlyName" => "Project password", "Type" => "text", "Size" => "32", "Description" => "Enter unique sign password",),
+        "testmode"     => array("FriendlyName" => "Test Mode", "Type" => "yesno", "Description" => "Tick this to enable test mode",),
     );
-    return $configarray;
 }
 
 function paysera_link($params) {
-if(!$params['clientdetails']['email']){ return ''; }
-    $URL = array();
-
-    $URL['accept']   = $params['systemurl'] . '/modules/gateways/callback/paysera.php?accepturl=1';
-    $URL['cancel']   = $params['systemurl'] . '/clientarea.php';
-    $URL['callback'] = $params['systemurl'] . '/modules/gateways/callback/paysera.php';
+    if ( ! $params['clientdetails']['email'] ) return;
     try {
-        WebToPay::redirectToPayment(array(
+        $request = WebToPay::buildRequest( array(
             'projectid'     => $params['projectID'],
             'sign_password' => $params['projectPass'],
-
             'orderid'       => $params['invoiceid'],
-            'amount'        => intval(number_format($params['amount'], 2, '', '')),
+            'amount'        => intval( number_format($params['amount'], 2, '', '') ),
             'currency'      => $params['currency'],
-
-            'accepturl'     => $URL['accept'],
-            'cancelurl'     => $URL['cancel'],
-            'callbackurl'   => $URL['callback'],
-
+            'accepturl'     => $params['systemurl'] . '/modules/gateways/callback/paysera.php?accepturl=1',
+            'cancelurl'     => $params['systemurl'] . '/clientarea.php',
+            'callbackurl'   => $params['systemurl'] . '/modules/gateways/callback/paysera.php',
             'p_firstname'   => $params['clientdetails']['firstname'],
             'p_lastname'    => $params['clientdetails']['lastname'],
             'p_email'       => $params['clientdetails']['email'],
@@ -43,7 +33,13 @@ if(!$params['clientdetails']['email']){ return ''; }
             'p_countrycode' => $params['clientdetails']['country'],
             'test'          => $params['testmode'] == 'on' ? 1 : 0,
         ));
-    } catch (WebToPayException $e) {
+        $code  = '<form method="get" action="' . WebToPay::getPaymentUrl() . '"">';
+        $code .= '<input type="hidden" name="data" value="' . $request["data"] . '">';
+        $code .= '<input type="hidden" name="sign" value="' . $request["sign"] . '">';
+        $code .= '<input type="submit" value="Pay now">';
+        $code .= '</form>';
+        return $code;
+    } catch ( WebToPayException $e ) {
         echo get_class($e) . ': ' . $e->getMessage();
     }
 }
